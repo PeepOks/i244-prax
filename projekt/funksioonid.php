0f55265daa4c7d6bfe320b5b26d2f0b9339f5a5d
Peep Oks <poks@enos.itcollege.ee>
2016-05-24 11:10:59 +0300

    Piltide lisamine galeriisse ja nende muutmine.

--- projekt/funksioonid.php
+++ projekt/funksioonid.php
@@ -72,13 +72,39 @@ function fetch_user_data($uname) {
 	}
 }
 
+function fetch_picture_data($p_id) {
+	global $connection;
+	$p_id = mysqli_real_escape_string($connection, $p_id);
+	$query = "SELECT * from poks_projekt_pildid WHERE id='".$p_id."';";
+	$result = mysqli_query($connection, $query) or die("$query - ".mysqli_error($connect));
+	
+	if(mysqli_num_rows($result) > 0 ){
+			$pildiinfo = array();
+			$pildiinfo = mysqli_fetch_assoc($result);
+			return $pildiinfo;
+	} else {
+		header("Location: ?page=change_p");
+	}
+}
+
 function display_gallery(){
 	
 	if (!isset($_SESSION['logged_in_user'])){
     	header("Location: ?page=login");
 	} else {
-		// siia on vaja funktsionaalsust
 		global $connection;
+		$kasutajainfo = fetch_user_data($_SESSION['logged_in_user']);
+		$query = "select * from poks_projekt_pildid where omaniku_id='".$kasutajainfo["id"]."';";
+		$result = mysqli_query($connection, $query) or die("$query - ".mysqli_error($connect));
+		//loome tühja massiivi
+		$pildid = array();
+		// hangime tulemusest andmed
+		//$puurid = mysqli_fetch_assoc($result) or die(mysql_error());
+	
+		while($pilt = mysqli_fetch_assoc($result)){
+			$pildid[] = $pilt;
+		}
+		
 		include_once('views/galerii.html');
 	}
 }
@@ -86,13 +112,25 @@ function display_gallery(){
 function add_picture(){
 	if (!isset($_SESSION['logged_in_user'])){
     	header("Location: ?page=login");
-	} else  if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+	} else if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 		
-		$lisatudfail = upload("pfile");	
+		$lisatudfail = upload_picture("pfile");
+		$kasutajainfo = fetch_user_data($_SESSION['logged_in_user']);
 		if (isset($_POST['pnimi'], $_FILES['pfile']) && $lisatudfail!="" && $_POST['pnimi']!="" && $_FILES['pfile']['name']!=""){
 			global $connection;
+			$pnimi=mysqli_real_escape_string($connection, $_POST['pnimi']);
+			$pfail=mysqli_real_escape_string($connection, $lisatudfail);
+			$pkomm=mysqli_real_escape_string($connection, $_POST['pkomm']);
+			
+			$query = "INSERT INTO poks_projekt_pildid (omaniku_id, pildinimi, pildifail, pildikomm) VALUES ('".$kasutajainfo["id"]."', '".$pnimi."', '".$pfail."', '".$pkomm."');";
+			echo $query;
+			$result = mysqli_query($connection, $query) or die("$query - ".mysqli_error($connection));
+			if(mysqli_insert_id($connection) > 0 ){       
+				header("Location: ?page=gallery");
+        	}
 		}
-		
+		include_once('views/lisapilt.html');
+	} else {
 		include_once('views/lisapilt.html');
 	}
 		
@@ -102,10 +140,36 @@ function change_picture(){
 	if (!isset($_SESSION['logged_in_user'])){
     	header("Location: ?page=login");
 	} else {
-		// siia on vaja funktsionaalsust
-		global $connection;
+		
+		if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['id'])){
+			$pildiinfo = fetch_picture_data($_GET['id']);
+			include_once('views/muudapilti.html');	
+		} else if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['id'])){
+			global $connection;
+			$pildiinfo = fetch_picture_data($_POST['id']);
+			
+			if ($_FILES["pfile"]["name"]==""){
+				$lisatudfail = $pildiinfo['pildifail'];
+			} else {
+				$lisatudfail = upload_picture("pfile");
+			}
+			
+			$pid=mysqli_real_escape_string($connection, $_POST['id']);
+			$pnimi=mysqli_real_escape_string($connection, $_POST['pnimi']);
+			$pfail=mysqli_real_escape_string($connection, $lisatudfail);
+			$pkomm=mysqli_real_escape_string($connection, $_POST['pkomm']);
+			
+			$query = "UPDATE poks_projekt_pildid SET pildinimi='".$pnimi."', pildifail='".$pfail."', pildikomm='".$pkomm."' WHERE id='".$pid."';";			
+			$result = mysqli_query($connection, $query) or die("$query - ".mysqli_error($connection));
+			if(mysqli_affected_rows($connection) > 0 ){
+				//Kui saime kasutaja loodud oleme ka sisse logitud ja lähme galeriisse.
+				header("Location: ?page=gallery");
+			}
+			include_once('views/muudakasutajat.html');
+		} else {
+			header("Location: ?page=gallery");
+		}
 
-		include_once('views/muudapilti.html');
 	}
 }
 
@@ -183,7 +247,6 @@ function tingimused(){
 function upload_picture($name){
 	$allowedExts = array("jpg", "jpeg", "gif", "png");
 	$allowedTypes = array("image/gif", "image/jpeg", "image/png","image/pjpeg");
-	//$extension = end(explode(".", $_FILES[$name]["name"]));
 	$parts = explode(".", $_FILES[$name]["name"]);
 	$extension = end($parts);
 	
@@ -196,14 +259,14 @@ function upload_picture($name){
 			return "";
 		} else {
       // vigu ei ole
-			if (file_exists("pildid/" . $_FILES[$name]["name"])) {
+			if (file_exists("pictures/".$_SESSION['logged_in_user']."_".$_FILES[$name]["name"])) {
         // fail olemas ära uuesti lae, tagasta failinimi
 				$_SESSION['notices'][]= $_FILES[$name]["name"] . " juba eksisteerib. ";
-				return "pildid/" .$_FILES[$name]["name"];
+				return "pictures/".$_SESSION['logged_in_user']."_".$_FILES[$name]["name"];
 			} else {
         // kõik ok, aseta pilt
-				move_uploaded_file($_FILES[$name]["tmp_name"], "pildid/" . $_FILES[$name]["name"]);
-				return "pildid/" .$_FILES[$name]["name"];
+				move_uploaded_file($_FILES[$name]["tmp_name"], "pictures/".$_SESSION['logged_in_user']."_".$_FILES[$name]["name"]);
+				return "pictures/".$_SESSION['logged_in_user']."_".$_FILES[$name]["name"];
 			}
 		}
 	} else {
